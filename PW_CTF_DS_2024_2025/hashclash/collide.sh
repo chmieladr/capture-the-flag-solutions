87d1f3d528209f80cab265389958a6bebf0a4ac6
chmieladr <achmiel309@gmail.com>
2024-12-12 23:30:13 +0100

    Made collide.sh script less misleading

--- PW_CTF_DS_2024_2025/hashclash/collide.sh
+++ PW_CTF_DS_2024_2025/hashclash/collide.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker run --rm -it -v $PWD:/work -w /work -u $UID:$GID brimstone/fastcoll --prefixfile prefix -o msg1.bin msg2.bin
-cat msg1.bin suffix > safe.py
-cat msg2.bin suffix > malicious.py
+docker run --rm -it -v "$PWD":/work -w /work -u $UID:"$GID" brimstone/fastcoll --prefixfile prefix -o msg1.bin msg2.bin
+cat msg1.bin suffix > prog1.py
+cat msg2.bin suffix > prog2.py
