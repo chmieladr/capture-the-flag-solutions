5bceaf0cbd708278c9cb7339811f40dddc8886a6
chmieladr <achmiel309@gmail.com>
2025-02-07 04:18:35 +0100

    Uncommented try/catch in parallel_decrypt/main.py (oops) & updated README.md

--- PW_CTF_DS_2024_2025/cwe327/parallel_decrypt/main.py
+++ PW_CTF_DS_2024_2025/cwe327/parallel_decrypt/main.py
@@ -4,6 +4,7 @@ from executors import encrypt_ecb_parallel, decrypt_ecb_parallel, \
     encrypt_cbc, decrypt_cbc_parallel, process_ctr_parallel
 from utils import usage_message, iv_message, nonce_message
 
+# Adjust to your system/needs
 num_processes = 16
 
 if __name__ == '__main__':
@@ -15,44 +16,44 @@ if __name__ == '__main__':
     input_file = argv[1]
     output_file = argv[2]
 
-    # try:
-    input_text = open(input_file, 'rb').read()
-    key = argv[3].encode()
+    try:
+        input_text = open(input_file, 'rb').read()
+        key = argv[3].encode()
 
-    if argv[4] == '1':
-        result = encrypt_ecb_parallel(key, input_text, num_processes=num_processes)
-    elif argv[4] == '2':
-        result = decrypt_ecb_parallel(key, input_text, num_processes=num_processes)
-    elif argv[4] == '3':
-        if len(argv) != 6:
-            print(iv_message, file=stderr)
-            exit(2)
-        iv = argv[5]
-        result = encrypt_cbc(key, input_text, iv)
-    elif argv[4] == '4':
-        if len(argv) != 6:
-            print(iv_message, file=stderr)
-            exit(2)
-        iv = argv[5]
-        result = decrypt_cbc_parallel(key, input_text, iv, num_processes=num_processes)
-    elif argv[4] == '5':
-        if len(argv) != 6:
-            print(nonce_message, file=stderr)
-            exit(3)
-        nonce = argv[5].encode()
-        result = process_ctr_parallel(key, input_text, nonce, num_processes=num_processes)
-    else:
-        print('Invalid mode!', file=stderr)
-        exit(4)
-    # except (OSError, FileNotFoundError):
-    #     print('File operation error!', file=stderr)
-    #     exit(5)
-    # except (OverflowError, ValueError):
-    #     print('Invalid key, IV or nonce!', file=stderr)
-    #     exit(6)
-    # except TypeError as e:
-    #     print(e, file=stderr)
-    #     exit(7)
+        if argv[4] == '1':
+            result = encrypt_ecb_parallel(key, input_text, num_processes=num_processes)
+        elif argv[4] == '2':
+            result = decrypt_ecb_parallel(key, input_text, num_processes=num_processes)
+        elif argv[4] == '3':
+            if len(argv) != 6:
+                print(iv_message, file=stderr)
+                exit(2)
+            iv = argv[5]
+            result = encrypt_cbc(key, input_text, iv)
+        elif argv[4] == '4':
+            if len(argv) != 6:
+                print(iv_message, file=stderr)
+                exit(2)
+            iv = argv[5]
+            result = decrypt_cbc_parallel(key, input_text, iv, num_processes=num_processes)
+        elif argv[4] == '5':
+            if len(argv) != 6:
+                print(nonce_message, file=stderr)
+                exit(3)
+            nonce = argv[5].encode()
+            result = process_ctr_parallel(key, input_text, nonce, num_processes=num_processes)
+        else:
+            print('Invalid mode!', file=stderr)
+            exit(4)
+    except (OSError, FileNotFoundError):
+        print('File operation error!', file=stderr)
+        exit(5)
+    except (OverflowError, ValueError):
+        print('Invalid key, IV or nonce!', file=stderr)
+        exit(6)
+    except TypeError as e:
+        print(e, file=stderr)
+        exit(7)
 
     output = open(output_file, 'wb')
     output.write(result)
--- README.md
+++ README.md
@@ -3,13 +3,23 @@ Repository that contains all of my solutions to **Capture The Flag** competition
 
 ## Files structure
 In each subdirectory corresponding to a specific challenge, you might be able to find the following files:
-- `container` subdirectory - files that I was able to export/download out of the container that was provided to me during the challenge (so you can have a better idea of what I was working with)
+- `container` subdirectory - files that I was able to export/download out of the container that was provided to me
+during the challenge (so you can have a better idea of what I was working with)
 - `task.md` - a description of the task that I was supposed to solve
 - `solution.md` - a detailed explanation of the solution that I have come up with
-- other files that I have created/used during the challenge (their purpose is usually explained somewhere inside the particular task)
+- other files that I have created/used during the challenge
+(their purpose is usually explained somewhere inside the particular task or can be determined by common sense)
+
+## `utils` directory
+This directory contains some programs/scripts that may come useful during the future challenges.
+They are not directly related to any of the challenges existing in this current repository.
 
 ### EE_CTF 2024
-CTF organised by Electrical Faculty at WUT mostly for Applied Computer Science students during summer holidays. I managed to complete 6 out of 9 tasks.
+CTF that was organized by Electrical Faculty at WUT mostly for Applied Computer Science students during summer holidays.
+I managed to complete six out of nine tasks.
 
 ### PW_CTF_DS 2024-2025
-CTF organised as a part of "Data Security" course in the field of Applied Computer Science at WUT. While this challenge wasn't obligatory for all of us to actually participate, it provided juicy additional points, and I was totally down for it. I have completed all 5 out of 5 tasks (excluding the test one as that wasn't really a challenge for anyone).
+CTF organised as a part of "Data Security" course in the field of Applied Computer Science at WUT.
+While this challenge wasn't obligatory for all of us to actually participate, it provided juicy additional points,
+and I was totally down for it.
+I have completed all five out of five tasks (excluding the test one as that wasn't really a challenge for anyone).
